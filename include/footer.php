88ef0fdcb23f39deb4fe6c03dbc383992162b88b
Aisha <AishaaMalik@hotmail.com>
2022-02-10 01:15:55 +0100

    git init

---
+++ include/footer.php
@@ -0,0 +1,22 @@
+
+
+<div class="footer">
+
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+
+</div>
---
+++ include/navigation.php
@@ -0,0 +1,8 @@
+
+<div class="Navigation">
+    <a href="index.php">Forside</a>
+    <a href="#">Om os</a>
+    <a href="contact.php">Kontakt</a>
+    <a href="#">Ny side</a>
+</div>
+
